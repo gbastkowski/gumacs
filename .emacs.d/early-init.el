2f66953718666f95748b83cb7d781e56123bef78
Gunnar Bastkowski <gunnar@bastkowski.name>
2023-02-18 00:21:04 +0100

    Initial commit

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,6 @@
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(tooltip-mode -1)
+(setq package-enable-at-startup nil) ;; Disable package.el in favor of straight.el
+
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+HOME=/Users/gunnar.bastkowski/git/gbastkowski/emacs \
+    /opt/homebrew/Cellar/emacs-plus@29/29.0.60/Emacs.app/Contents/MacOS/Emacs \
+    --debug-init
